e7f74a9a92f8b28c635df3e22c317a190b34e7f0
江夏尧 <3446798488@qq.com>
2023-05-09 09:54:25 +0800

    feat: 补充内部中间件的参数

--- mod.ts
+++ mod.ts
@@ -1,10 +1,18 @@
+import type { KoaBodyMiddlewareOptions } from "https://esm.sh/koa-body@6.0.1";
 import { Router, koaBody, cors, Koa, logger } from "./deps.ts";
 import { APIConfig, SuperRouter } from "./utils/Modules.ts";
 export * from "./utils/Modules.ts";
 export * from "./deps.ts";
 /** 直接初始化服务器 */
 export const ServerInit = (
-    modules: Record<string, Record<string, APIConfig>>
+    modules: Record<string, Record<string, APIConfig>>,
+    config?: {
+        /** 用于使用 koa 的中间件 */
+        extends?: (app: Koa<Koa.DefaultState, Koa.DefaultContext>) => void;
+        cors?: cors.Options;
+        body?: Partial<KoaBodyMiddlewareOptions>;
+        apiPrefix?: string;
+    }
 ) => {
     const router = new Router();
     const subRouter = new Router();
@@ -16,27 +24,41 @@ export const ServerInit = (
         );
     });
 
-    router.use("/api", subRouter.routes());
+    router.use(config?.apiPrefix ?? "/api", subRouter.routes());
     const app = new Koa();
-    app.use(async (ctx, next) => {
-        try {
-            await next();
-        } catch (err) {
-            ctx.status = err.status || 500;
-            ctx.body = err.message;
-            ctx.app.emit("error", err, ctx);
+    config && config.extends && config.extends(app);
+    app.use(
+        // 错误拦截
+        async (ctx, next) => {
+            try {
+                await next();
+            } catch (err) {
+                ctx.status = err.status || 500;
+                ctx.body = { status: err.status, error: err.message };
+                ctx.app.emit("error", err, ctx);
+            }
         }
-    })
+    )
         .use(logger())
         .use(
-            cors({
-                origin: "*",
-            })
+            cors(
+                Object.assign(
+                    {
+                        origin: "*",
+                    },
+                    config?.cors
+                )
+            )
         )
         .use(
-            koaBody({
-                multipart: true,
-            })
+            koaBody(
+                Object.assign(
+                    {
+                        multipart: true,
+                    },
+                    config?.body
+                )
+            )
         )
         .use(router.routes())
         .use(router.allowedMethods());
